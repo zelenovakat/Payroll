24f6a68d68d67a2bfe3ee5fc992e5e8e3f87502e
Екатерина Зеленова <ekaterinazelenova@MacBook-Air-Ekaterina.local>
2021-12-07 19:26:44 +0100

    creating slides, button for slides, installing styled-components, style for button and slides

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 
 const Button = ({ children, onClick }) => {
   const handleButtonClick = (e) => {
@@ -7,10 +8,26 @@ const Button = ({ children, onClick }) => {
   }
 
   return (
-    <button type="button" onClick={handleButtonClick}>
+    <StyledButton type="button" onClick={handleButtonClick}>
       {children}
-    </button>
+    </StyledButton>
   )
 }
 
 export default Button
+
+const StyledButton = styled.button`
+  background: #fff;
+  border: none;
+  border-radius: 3px;
+  color: #961f8c;
+  transition: background 0.2s ease-in-out;
+  min-height: 30px;
+  min-width: 150px;
+  margin: 16px 0;
+
+  &:hover {
+    background: #c524b8;
+    color: #fff;
+  }
+`
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import styled from "styled-components"
 import SelectExperience from "./dropdownList/SelectExperience"
 import SelectProfession from "./dropdownList/SelectProfession"
 import SelectYear from "./radioButtonList/SelectYear"
@@ -16,6 +17,11 @@ const Form = () => {
   const [city, setCity] = useState("")
   const [totalSalaryWithTax, setTotalSalaryWithTax] = useState(0)
   const [tax, setTax] = useState(0)
+  const [activeSlide, setActiveSlide] = useState(1)
+
+  const nextSlide = () => {
+    setActiveSlide(activeSlide + 1)
+  }
 
   const calculateSalary = () => {
     const basicSalaryByProfession = basicSalaries[profession.toLowerCase()]
@@ -28,31 +34,68 @@ const Form = () => {
 
     const calculatedTax = calculateTax(city, year, total)
     setTax(calculatedTax)
+
+    nextSlide()
   }
 
   return (
     <div>
-      <div>
-        <SelectProfession profession={profession} setProfession={setProfession} />
-      </div>
-      <div>
-        <SelectExperience
-          yearsOfExperience={yearsOfExperience}
-          setYearsOfExperience={setYearsOfExperience}
-        />
-      </div>
-      <div>
-        <SelectYear year={year} setYear={setYear} />
-      </div>
-      <div>
-        <SelectCity city={city} setCity={setCity} />
-      </div>
-      <Button onClick={calculateSalary}>Calculate</Button>
-      <h2>Salary ex Tax:</h2>
-      <h2>TAX: {tax} kr</h2>
-      <h2>Total salary: {totalSalaryWithTax} kr</h2>
+      {activeSlide === 1 && (
+        <Slide>
+          <h1>How much could you earn?</h1>
+          <Button onClick={nextSlide}>Start</Button>
+        </Slide>
+      )}
+      {activeSlide === 2 && (
+        <Slide>
+          <SelectProfession profession={profession} setProfession={setProfession} />
+          <Button onClick={nextSlide}>Next</Button>
+        </Slide>
+      )}
+      {activeSlide === 3 && (
+        <Slide>
+          <SelectExperience
+            yearsOfExperience={yearsOfExperience}
+            setYearsOfExperience={setYearsOfExperience}
+          />
+          <Button onClick={nextSlide}>Next</Button>
+        </Slide>
+      )}
+      {activeSlide === 4 && (
+        <Slide>
+          <SelectYear year={year} setYear={setYear} />
+          <Button onClick={nextSlide}>Next</Button>
+        </Slide>
+      )}
+      {activeSlide === 5 && (
+        <Slide>
+          <SelectCity city={city} setCity={setCity} />
+          <Button onClick={calculateSalary}>Calculate</Button>
+        </Slide>
+      )}
+      {activeSlide === 6 && (
+        <Slide>
+          <h2>Salary ex Tax: {totalSalaryWithTax - tax} kr</h2>
+          <h2>TAX: {tax} kr</h2>
+          <h2>Total salary: {totalSalaryWithTax} kr</h2>
+
+          <Button onClick={() => setActiveSlide(1)}>Start over</Button>
+        </Slide>
+      )}
     </div>
   )
 }
 
 export default Form
+
+const Slide = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #262626;
+  color: #fff;
+`
